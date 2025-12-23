92cbd44fc1aad8a96df7471d6c14959f7d88a214
Rithika Korrapolu <rkrox24@gmail.com>
2025-12-22 23:30:11 -0500

    Add apiVersion to Shopify client configuration

--- lib/shopify.ts
+++ lib/shopify.ts
@@ -3,6 +3,7 @@ import Client from 'shopify-buy';
 const client = Client.buildClient({
   domain: process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN!,
   storefrontAccessToken: process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN!,
+  apiVersion: '2024-01',
 });
 
 export async function getProducts() {
